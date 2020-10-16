bf9ba157011ec2f5016abde9c1c98f5bc617265c
Mrinal <mkmrinal768@gmail.com>
2020-10-17 00:51:52 +0100

    flake8 recomendation

--- app/app/test.py
+++ app/app/test.py
@@ -1,13 +1,14 @@
 from django.test import TestCase
 from app.calc import add
 
-  class CalcTest(TestCase):
-      ''' To test the add functionality '''
 
-      def test_integerNumbers(self):
-          ''' Test Integer values '''
-          self.assertEqual(add(3 , 8) , 11)
+class CalcTest(TestCase):
+    ''' To test the add functionality '''
 
-      def test_decimalNumbers(self):
-          ''' Test decimal numbers '''
-          self.assertEqual(add(2.5 , 2.5) , 5.0)
+    def test_integerNumbers(self):
+        ''' Test Integer values '''
+        self.assertEqual(add(3 , 8) , 11)
+
+    def test_decimalNumbers(self):
+        ''' Test decimal numbers '''
+        self.assertEqual(add(2.5 , 2.5) , 5.0)
